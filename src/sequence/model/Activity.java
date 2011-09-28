5adc49f4d19604bacad32f74b31aaf5d85925ef9
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-09-28 21:52:11 +0200

    Activity implementation.

---
+++ src/sequence/model/Activity.java
@@ -0,0 +1,85 @@
+package sequence.model;
+
+public class Activity {
+	private final int id;
+	private final String state;
+	private final int discipline;
+	private final int type;
+	
+	private Activitytime activitytime;
+	private Actuator actuator;
+	private Actions actions;
+	private UsedInstruments usedInstrument;
+	private TreatedStructure treatedStructure;
+	private Note note;
+
+	
+	public Activity(int id, String state, int discipline, int type) {
+		this.id=id;
+		this.state=state;
+		this.discipline=discipline;
+		this.type=type;
+		
+		this.activitytime=new Activitytime();
+		this.actuator=new Actuator();
+		this.actions=new Actions();
+		this.usedInstrument=new UsedInstruments();
+		this.treatedStructure=new TreatedStructure();
+	}
+
+
+	public Note getNote() {
+		return note;
+	}
+
+	public void setNote(Note note) {
+		this.note = note;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public int getDiscipline() {
+		return discipline;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public Activitytime getActivitytime() {
+		return activitytime;
+	}
+
+	public Actuator getActuator() {
+		return actuator;
+	}
+
+	public Actions getActions() {
+		return actions;
+	}
+
+	public UsedInstruments getUsedInstrument() {
+		return usedInstrument;
+	}
+
+	public TreatedStructure getTreatedStructure() {
+		return treatedStructure;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", state=" + state + ", discipline="
+				+ discipline + ", type=" + type + ", activitytime="
+				+ activitytime + ", actuator=" + actuator + ", actions="
+				+ actions + ", usedInstrument=" + usedInstrument
+				+ ", treatedStructure=" + treatedStructure + ", note=" + note
+				+ "]";
+	}
+	
+}
