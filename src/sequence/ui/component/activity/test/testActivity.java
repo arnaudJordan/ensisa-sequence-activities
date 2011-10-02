a36e075c9247b75d08497b98c811e90d8d8b6ac7
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-02 21:49:07 +0200

    Added testActivity

---
+++ src/sequence/ui/component/activity/test/testActivity.java
@@ -0,0 +1,39 @@
+package sequence.ui.component.activity.test;
+
+import java.io.File;
+
+import javax.swing.JFrame;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import sequence.model.Sequence;
+import sequence.parser.SequenceHandler;
+import sequence.ui.component.activity.ActivityView;
+
+public class testActivity extends JFrame {
+
+	public testActivity() {
+		try{
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+
+			SAXParser parser = factory.newSAXParser();
+
+			File parsedFile = new File("src/data.xml");
+			SequenceHandler sequenceHandler = new SequenceHandler();
+			parser.parse(parsedFile, sequenceHandler);
+			
+			Sequence model = new Sequence(sequenceHandler.getSequence());
+			ActivityView view = new ActivityView(model.getLastActivity());
+			this.add(view);
+			this.pack();
+			this.setVisible(true);
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		new testActivity();
+	}
+}
