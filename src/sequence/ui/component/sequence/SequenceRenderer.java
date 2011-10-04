b81021d2e3e78a0795cf80c598da05ccaa29c8e4
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-04 12:36:39 +0200

    Factorized code in Renderers

--- src/sequence/ui/component/sequence/SequenceRenderer.java
+++ src/sequence/ui/component/sequence/SequenceRenderer.java
@@ -21,9 +21,7 @@ public class SequenceRenderer extends DefaultRenderer implements Renderer {
 	}
 
 	public void renderView(Graphics2D g) {
-		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); 
-        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY); 
-        g.setRenderingHints(rh);
+		super.renderView(g);
         renderSequence(g);
 	}
 	
