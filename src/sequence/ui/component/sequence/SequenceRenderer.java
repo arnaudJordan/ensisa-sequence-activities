79972f09fcbb448a0a7687530e354730e09f2e13
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-05 00:01:18 +0200

    Improved the way activityViews are added in a sequenceView

--- src/sequence/ui/component/sequence/SequenceRenderer.java
+++ src/sequence/ui/component/sequence/SequenceRenderer.java
@@ -1,12 +1,8 @@
 package sequence.ui.component.sequence;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-
 import sequence.model.Activity;
 import sequence.model.Sequence;
 import sequence.mvc.DefaultRenderer;
@@ -18,6 +14,9 @@ public class SequenceRenderer extends DefaultRenderer implements Renderer {
 	
 	public SequenceRenderer(View view) {
 		super(view);
+		getView().setLayout(new FlowLayout());
+		for(Activity current : ((Sequence)getView().getModel()).getActivities()) 
+			getView().add(new ActivityView(current));
 	}
 
 	public void renderView(Graphics2D g) {
@@ -26,21 +25,6 @@ public class SequenceRenderer extends DefaultRenderer implements Renderer {
 	}
 	
 	private void renderSequence(Graphics2D g) {
-		getView().setLayout(new FlowLayout());
-		for(Activity current : ((Sequence)getView().getModel()).getActivities()) {
-			boolean flag = false;
-			if(getView().getComponentCount() != 0) {
-				int i = 0;
-				while(!flag && i < getView().getComponents().length) {
-					if(((Activity)(((ActivityView)getView().getComponents()[i]).getModel())).getId() != current.getId())
-						i++;
-					else
-						flag = true;
-				}
-			}
-			if(!flag)
-				getView().add(new ActivityView(current));
-		}
 		System.out.println(getView().getComponentCount());
 	}
 	
