0c362b49738fce2a75a001d6766b154bb5c09593
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-08 18:22:21 +0200

    Added height field in ActivityRenderingModel which determines the height of an ActivityView

--- src/sequence/ui/component/activity/ActivityRenderingModel.java
+++ src/sequence/ui/component/activity/ActivityRenderingModel.java
@@ -7,15 +7,18 @@ import sequence.mvc.RenderingModel;
 public class ActivityRenderingModel implements RenderingModel {
 
 	private static final Color DEFAULT_COLOR = Color.BLACK;
+	private static final int DEFAULT_HEIGHT = 10;
 	
 	private Color color;
+	private int height;
 	
 	public ActivityRenderingModel() {
-		this(DEFAULT_COLOR);
+		this(DEFAULT_COLOR, DEFAULT_HEIGHT);
 	}
 	
-	public ActivityRenderingModel(Color color) {
+	public ActivityRenderingModel(Color color, int height) {
 		setColor(color);
+		setHeight(height);
 	}
 
 	public Color getColor() {
@@ -24,5 +27,13 @@ public class ActivityRenderingModel implements RenderingModel {
 
 	public void setColor(Color color) {
 		this.color = color;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 }
