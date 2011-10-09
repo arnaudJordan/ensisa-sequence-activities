4dd1c66768514ec9afc9c232fbec438b082c1bf4
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-09 22:43:34 +0200

    Merge with 3e0886a9f41ac0ef941b79cd062c667d8a3e1439

--- src/sequence/ui/component/activity/ActivityController.java
+++ src/sequence/ui/component/activity/ActivityController.java
@@ -14,11 +14,14 @@ public class ActivityController extends Controller {
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		ActivityRenderingModel renderingModel = ((ActivityRenderingModel)(((ActivityView)getView()).getRenderingModel()));
-		Color newColor = JColorChooser.showDialog(getView(), "Choose a new color", renderingModel.getColor());
-		if(newColor!= null && newColor != renderingModel.getColor()) {
-			renderingModel.setColor(newColor);
-			getView().repaint();
+		if(e.getButton() == MouseEvent.BUTTON1)
+		{
+			ActivityRenderingModel renderingModel = ((ActivityRenderingModel)(((ActivityView)getView()).getRenderingModel()));
+			Color newColor = JColorChooser.showDialog(getView(), "Choose a new color", renderingModel.getColor());
+			if(newColor!= null && newColor != renderingModel.getColor()) {
+				renderingModel.setColor(newColor);
+				getView().repaint();
+			}
 		}
 	}
 }
