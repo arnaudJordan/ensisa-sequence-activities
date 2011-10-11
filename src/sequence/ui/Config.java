9f8b57d7752d3cfe2cd005fc6bd317b9b6ff8e7d
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-10-11 10:21:05 +0200

    Implement serialize and deserialize functions in Config file.

--- src/sequence/ui/Config.java
+++ src/sequence/ui/Config.java
@@ -1,9 +1,16 @@
 package sequence.ui;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Config implements Serializable{
+	private final String FILENAME="config.ser";
 	private File lastOpenedDirectory;
 
 	public File getLastOpenedDirectory() {
@@ -12,5 +19,20 @@ public class Config implements Serializable{
 
 	public void setLastOpenedDirectory(File lastOpenedDirectory) {
 		this.lastOpenedDirectory = lastOpenedDirectory;
+	}
+	
+	public void serialize() throws IOException
+	{
+		FileOutputStream configFile = new FileOutputStream(FILENAME);
+    	ObjectOutputStream oos = new ObjectOutputStream(configFile);
+    	oos.writeObject(MainWindow.getConfig());
+    	oos.flush();
+    	oos.close();
+	}
+	public Config deserialize() throws IOException, ClassNotFoundException
+	{
+		FileInputStream configFile = new FileInputStream(FILENAME);
+		ObjectInputStream ois = new ObjectInputStream(configFile);
+		return ((Config) ois.readObject());
 	}
 }
