ee044a573108763545341c3459f2dfc66436909b
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-11 11:27:54 +0200

    Improved blue color generation by comparing UsedInstruments rather than Instrument

--- src/sequence/utilities/ColorFactory.java
+++ src/sequence/utilities/ColorFactory.java
@@ -7,33 +7,32 @@ import sequence.model.Action;
 import sequence.model.Activity;
 import sequence.model.AnatomicStructure;
 import sequence.model.Instrument;
+import sequence.model.UsedInstruments;
 
 public class ColorFactory {
 
 	private List<Action> actions;
 	private List<AnatomicStructure> anatomicStructures;
-	private List<Instrument> usedInstruments;
+	private List<UsedInstruments> usedInstruments;
 	
 	public ColorFactory(List<Activity> activities) {
 		actions = new ArrayList<Action>();
 		anatomicStructures = new ArrayList<AnatomicStructure>();
-		usedInstruments = new ArrayList<Instrument>();
+		usedInstruments = new ArrayList<UsedInstruments>();
 		for(Activity current : activities) {
 			if(!actions.contains(current.getAction()))
 				actions.add(current.getAction());
 			if(!anatomicStructures.contains(current.getTreatedStructure()))
 				anatomicStructures.add(current.getTreatedStructure());
-			for(Instrument instrument : current.getUsedInstrument().getUsedInstruments()) {
-				if(!usedInstruments.contains(instrument))
-					usedInstruments.add(instrument);
-			}
+			if(!usedInstruments.contains(current.getUsedInstrument()))
+				usedInstruments.add(current.getUsedInstrument());
 		}
 	}
 	
 	public Color createColor(Activity activity) {
 		int red = 255 / actions.size() * actions.indexOf(activity.getAction());
 		int green = 255 / anatomicStructures.size() * anatomicStructures.indexOf(activity.getTreatedStructure());
-		int blue = 255 / usedInstruments.size() * usedInstruments.indexOf(activity.getUsedInstrument().getUsedInstruments().get(0));
+		int blue = 255 / usedInstruments.size() * usedInstruments.indexOf(activity.getUsedInstrument());
 		return new Color(red, green, blue);
 	}
 }
