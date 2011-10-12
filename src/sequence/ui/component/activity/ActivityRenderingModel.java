0cd28700ec75b1f6220c18d8f341a54b96a8c786
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-12 21:59:03 +0200

    Added scale in ActivityRenderingModel, il will allow us to change the size of an activityView

--- src/sequence/ui/component/activity/ActivityRenderingModel.java
+++ src/sequence/ui/component/activity/ActivityRenderingModel.java
@@ -9,40 +9,53 @@ public class ActivityRenderingModel extends DefaultModel implements RenderingMod
 
 	private static final Color DEFAULT_COLOR = Color.BLACK;
 	private static final int DEFAULT_HEIGHT = 10;
+	private static final float DEFAULT_SCALE = 1;
 	
 	private Color color;
 	private int height;
+	private float scale;
 	
 	public ActivityRenderingModel() {
-		this(DEFAULT_COLOR, DEFAULT_HEIGHT);
+		this(DEFAULT_COLOR, DEFAULT_HEIGHT, DEFAULT_SCALE);
 	}
 	
 	public ActivityRenderingModel(Color color) {
-		this(color, DEFAULT_HEIGHT);
+		this(color, DEFAULT_HEIGHT, DEFAULT_SCALE);
 	}
 	
-	public ActivityRenderingModel(Color color, int height) {
+	public ActivityRenderingModel(Color color, int height, float scale) {
 		setColor(color);
 		setHeight(height);
+		setScale(scale);
+	}
+	
+	public void setColor(Color color) {
+		if(this.color == color) return;
+		this.color = color;
+		this.modelChange();
 	}
 
 	public Color getColor() {
 		return color;
 	}
 
-	public void setColor(Color color) {
-		if(this.color == color) return;
-		this.color = color;
+	public void setHeight(int height) {
+		if(this.height == height) return;
+		this.height = height;
 		this.modelChange();
 	}
 	
 	public int getHeight() {
 		return height;
 	}
-	
-	public void setHeight(int height) {
-		if(this.height == height) return;
-		this.height = height;
+
+	public void setScale(float scale) {
+		if(this.scale == scale) return;
+		this.scale = scale;
 		this.modelChange();
+	}
+
+	public float getScale() {
+		return scale;
 	}
 }
