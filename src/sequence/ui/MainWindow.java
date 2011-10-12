44b4caa46fe0dc53c09c1ab1824a6d4718fda38c
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-10-12 18:51:58 +0200

    MenuBar and MainWindow interaction correction.

--- src/sequence/ui/MainWindow.java
+++ src/sequence/ui/MainWindow.java
@@ -2,21 +2,13 @@ package sequence.ui;
 
 import java.awt.Dimension;
 import java.awt.HeadlessException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-
 import javax.swing.JFrame;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import sequence.model.Sequence;
-import sequence.parser.SequenceHandler;
-import sequence.ui.component.sequence.SequenceView;
 
 public class MainWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private static Config config;
+	private Config config;
+	private Sequence sequence;
 
 	public MainWindow(String title) throws HeadlessException {
 		super(title);
@@ -31,36 +23,26 @@ public class MainWindow extends JFrame {
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-
-		this.setJMenuBar(new MenuBar());
-		try{
-			SAXParserFactory factory = SAXParserFactory.newInstance();
-
-			SAXParser parser = factory.newSAXParser();
-
-			File parsedFile = new File("src/data.xml");
-			SequenceHandler sequenceHandler = new SequenceHandler();
-			parser.parse(parsedFile, sequenceHandler);
-
-			Sequence model = new Sequence(sequenceHandler.getSequence());
-			SequenceView view = new SequenceView(model);
-
-			this.add(view);
-
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		this.setJMenuBar(new MenuBar(this));
 
 		this.pack();
 		this.setVisible(true);
 	}
 
-	public static Config getConfig() {
+	public Config getConfig() {
 		return config;
 	}
 
-	public static void setConfig(Config config) {
-		MainWindow.config = config;
+	public void setConfig(Config config) {
+		this.config = config;
+	}
+
+	public Sequence getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(Sequence sequence) {
+		this.sequence = sequence;
 	}
 
 
