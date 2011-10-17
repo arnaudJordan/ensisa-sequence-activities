fd1443da250a5ff8c1aa0474faf6a9bb37c7cabf
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-17 15:53:42 +0200

    Change color of every ActivityViews with the same color as the one getting change.

--- src/sequence/ui/component/activity/ActivityController.java
+++ src/sequence/ui/component/activity/ActivityController.java
@@ -1,6 +1,7 @@
 package sequence.ui.component.activity;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.event.MouseEvent;
 import javax.swing.JColorChooser;
 import sequence.mvc.Controller;
@@ -16,9 +17,18 @@ public class ActivityController extends Controller {
 	public void mouseClicked(MouseEvent e) {
 		if(e.getButton() == LEFT_MOUSE_BUTTON) {
 			ActivityRenderingModel renderingModel = ((ActivityRenderingModel)(((ActivityView)getView()).getRenderingModel()));
-			Color newColor = JColorChooser.showDialog(getView(), "Choose a new color", renderingModel.getColor());
-			if(newColor!= null && newColor != renderingModel.getColor())
+			Color oldColor = renderingModel.getColor();
+			Color newColor = JColorChooser.showDialog(getView(), "Choose a new color", oldColor);
+			if(newColor!= null && !newColor.equals(oldColor))
 				renderingModel.setColor(newColor);
+			Container parent = getView().getParent();
+			if(parent != null) {
+				for(int i=0 ; i<parent.getComponentCount() ; i++) {
+					ActivityRenderingModel componentRenderingModel = ((ActivityRenderingModel)((View)parent.getComponent(i)).getRenderingModel());
+					if(componentRenderingModel != null && componentRenderingModel.getColor().equals(oldColor))
+						componentRenderingModel.setColor(newColor);
+				}
+			}
 		}
 	}
 	
