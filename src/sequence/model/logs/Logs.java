4704cb2652d31f0de6b1731c72a7f827290ffb8a
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-10-27 17:35:08 +0200

    Add Logs model.

---
+++ src/sequence/model/logs/Logs.java
@@ -0,0 +1,55 @@
+package sequence.model.logs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import sequence.model.activity.Instrument;
+
+public class Logs {
+	private List<EditEvent> editEvents;
+	
+	public Logs()
+	{
+		this.editEvents = new ArrayList<EditEvent>();
+	}
+
+	public int size() {
+		return editEvents.size();
+	}
+
+	public boolean isEmpty() {
+		return editEvents.isEmpty();
+	}
+
+	public boolean contains(Object o) {
+		return editEvents.contains(o);
+	}
+
+	public Iterator<EditEvent> iterator() {
+		return editEvents.iterator();
+	}
+
+	public boolean add(EditEvent e) {
+		return editEvents.add(e);
+	}
+	
+	public EditEvent getLastEditEvent()
+	{
+		return editEvents.get(editEvents.size()-1);
+	}
+
+	public String toXML() {
+		final String NEW_LINE = System.getProperty("line.separator");
+		StringBuilder sb = new StringBuilder("<sw-logs>"+NEW_LINE);
+		for(EditEvent editEvent : editEvents)
+			sb.append("\t"+editEvent.toXML()+NEW_LINE);
+		sb.append("</sw-logs>");
+		return sb.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Logs [editEvents=" + editEvents + "]";
+	}
+}
