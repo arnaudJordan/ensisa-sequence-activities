e6023f6a383fcdb1de66bce71395ccbf164838f8
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-10-27 18:52:13 +0200

    Add SequenceContainer and close button to each sequence.

---
+++ src/sequence/ui/component/sequence/SequenceContainer.java
@@ -0,0 +1,44 @@
+package sequence.ui.component.sequence;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import sequence.ui.MainWindow;
+
+public class SequenceContainer extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private SequenceView sequenceView;
+	private MainWindow mainWindow;
+	
+	public SequenceContainer(final SequenceView sequenceView, final MainWindow mainWindow)
+	{
+		this.sequenceView = sequenceView;
+		this.mainWindow = mainWindow;
+		
+		setBackground(Color.WHITE);
+		setLayout(new BorderLayout());
+		
+		ImageIcon icon = new ImageIcon("icons/dialog-close.png");
+		JButton button = new JButton(icon);
+		final SequenceContainer sc= this;
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mainWindow.removeSequence(sc);
+			}
+		});
+		add(button, BorderLayout.PAGE_START);;
+		add(sequenceView, BorderLayout.CENTER);
+		setVisible(true);
+	}
+
+	public  SequenceView getSequenceView() {
+		return sequenceView;
+	}
+}
