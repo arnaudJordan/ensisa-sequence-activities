207b91acd1a8f4c03f27a9beedd55c6ada0fb484
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-10-27 15:15:01 +0200

    Error correction.

--- src/sequence/model/Participant.java
+++ src/sequence/model/Participant.java
@@ -86,18 +86,19 @@ public class Participant {
 	public String toXML() {
 		final String NEW_LINE = System.getProperty("line.separator");
 		StringBuilder sb = new StringBuilder("<participant>"+NEW_LINE);
-		if(clinicalDegree!=null)
+		if(position!=null)
 			sb.append("\t"+position.toXML()+NEW_LINE);
-		if(clinicalDegree!=null)
+		if(name!=null)
 			sb.append("\t"+name.toXML()+NEW_LINE);
 		if(clinicalDegree!=null)
 			sb.append("\t"+clinicalDegree.toXML()+NEW_LINE);
+		if(academicDegree!=null)
 		sb.append("\t"+academicDegree.toXML()+NEW_LINE);
 		for(Actor actor : actors)
 			sb.append("\t"+actor.toXML()+NEW_LINE);
-		if(clinicalDegree!=null)
+		if(note!=null)
 			sb.append("\t"+note.toXML()+NEW_LINE);
-		if(clinicalDegree!=null)
+		if(color!=null)
 			sb.append("\t"+color.toXML()+NEW_LINE);
 		sb.append("</participant>");
 		return sb.toString();
