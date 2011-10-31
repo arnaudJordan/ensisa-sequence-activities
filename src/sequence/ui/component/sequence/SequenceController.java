1e201d666879da87632281d3930cc5fa9b83f8cc
cyril.voegtlin <cyril.voegtlin@localhost>
2011-10-31 12:35:09 +0100

    Added popup menu to SequenceViews.

--- src/sequence/ui/component/sequence/SequenceController.java
+++ src/sequence/ui/component/sequence/SequenceController.java
@@ -1,12 +1,40 @@
 package sequence.ui.component.sequence;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 import sequence.mvc.Controller;
 import sequence.mvc.Model;
 import sequence.mvc.View;
 
-public class SequenceController extends Controller {
+public class SequenceController extends Controller implements ActionListener {
 
 	public SequenceController(Model model, View view) {
 		super(model, view);
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		try {
+			ImageIO.write(getView().createImage(), "png", new File("D:/User/test.png"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} 
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		checkPopup(e);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    	checkPopup(e);
+    }
+	
+	private void checkPopup(MouseEvent e) {
+        if (e.getButton() == RIGHT_MOUSE_BUTTON && e.isPopupTrigger()) {
+        	((SequenceView)getView()).getPopup().show(e.getComponent(), e.getX(), e.getY());
+        }
+    }
 }
--- src/sequence/ui/component/sequence/SequenceView.java
+++ src/sequence/ui/component/sequence/SequenceView.java
@@ -1,14 +1,27 @@
 package sequence.ui.component.sequence;
 
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
 import sequence.mvc.Model;
 import sequence.mvc.View;
 
 public class SequenceView extends View {
+	
+	private JPopupMenu popup;
 
 	public SequenceView(Model model) {
 		super(model);
 		setRenderingModel(new SequenceRenderingModel());
 		setRenderer(new SequenceRenderer(this));
+		popup = new JPopupMenu();
+	    JMenuItem exportItem = new JMenuItem("Export");
+	    exportItem.addActionListener(new SequenceController(getModel(), this));
+	    popup.add(exportItem);
+	}
+	
+	public JPopupMenu getPopup() {
+		return popup;
 	}
 	
 	public void modelChanged(Model m) {
