6d5eeed24a82b48ab55a73d615ad6eed6acb26df
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-11-03 13:42:37 +0100

    Create SequenceToSVG() in SVGFactory and add test.

--- src/sequence/ui/component/sequence/SequenceController.java
+++ src/sequence/ui/component/sequence/SequenceController.java
@@ -3,21 +3,26 @@ package sequence.ui.component.sequence;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+
 import javax.imageio.ImageIO;
 
 import sequence.mvc.Controller;
 import sequence.mvc.Model;
 import sequence.mvc.View;
 import sequence.ui.component.activity.ActivityRenderingModel;
+import sequence.ui.component.activity.ActivityView;
 import sequence.utilities.ColorFactory;
+import sequence.utilities.SVGFactory;
 
 public class SequenceController extends Controller implements ActionListener {
 
 	public SequenceController(Model model, View view) {
 		super(model, view);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		try {
 			ImageIO.write(getView().createImage(), "png", new File("D:/User/test.png"));
@@ -25,9 +30,22 @@ public class SequenceController extends Controller implements ActionListener {
 			ex.printStackTrace();
 		} 
 	}
-	
+
 	public void mouseClicked(MouseEvent e) {
+
 		if(e.getButton() == LEFT_MOUSE_BUTTON) {
+			try{
+				FileWriter fstream = new FileWriter("activity.svg");
+				BufferedWriter out = new BufferedWriter(fstream);
+				out.write(SVGFactory.AddHeader(SVGFactory.ActivityToSVG((ActivityView) getView().getComponent(0))));
+				out.close();
+				fstream = new FileWriter("sequence.svg");
+				out = new BufferedWriter(fstream);
+				out.write(SVGFactory.AddHeader(SVGFactory.SequenceToSVG((SequenceView) getView())));
+				out.close();
+			}catch (Exception e1){
+				System.err.println("Error: " + e1.getMessage());
+			}
 			ActivityRenderingModel componentRenderingModel;
 			for(int i=0 ; i<getView().getComponentCount() ; i++) {
 				componentRenderingModel = ((ActivityRenderingModel)((View)getView().getComponent(i)).getRenderingModel());
@@ -35,18 +53,18 @@ public class SequenceController extends Controller implements ActionListener {
 			}
 		}
 	}
-	
+
 	public void mousePressed(MouseEvent e) {
 		checkPopup(e);
-    }
+	}
 
-    public void mouseReleased(MouseEvent e) {
-    	checkPopup(e);
-    }
-	
+	public void mouseReleased(MouseEvent e) {
+		checkPopup(e);
+	}
+
 	private void checkPopup(MouseEvent e) {
-        if (e.getButton() == RIGHT_MOUSE_BUTTON && e.isPopupTrigger()) {
-        	((SequenceView)getView()).getPopup().show(e.getComponent(), e.getX(), e.getY());
-        }
-    }
+		if (e.getButton() == RIGHT_MOUSE_BUTTON && e.isPopupTrigger()) {
+			((SequenceView)getView()).getPopup().show(e.getComponent(), e.getX(), e.getY());
+		}
+	}
 }
