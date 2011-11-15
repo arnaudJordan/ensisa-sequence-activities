7f6b45c013345f9641751543d4d12cc9889be06f
jordan <jordan@localhost>
2011-11-15 13:47:38 +0100

    Better implementation.

--- src/sequence/processor/ColorChange.java
+++ src/sequence/processor/ColorChange.java
@@ -5,21 +5,20 @@ import sequence.ui.component.activity.ActivityRenderingModel;
 
 public class ColorChange extends Command {
 	private Color color;
-	private Color oldColor;
 	
 	public ColorChange(ActivityRenderingModel model, Color color)
 	{
 		this.model=model;
 		this.color=color;
-		this.oldColor=model.getColor();
+		this.undo=new ColorChange(model, color, this);
+	}
+	public ColorChange(ActivityRenderingModel model, Color color, ColorChange colorChange) {
+		this.model=model;
+		this.color=color;
+		this.undo=colorChange;
 	}
 	@Override
 	public void Do() {
 		((ActivityRenderingModel) this.model).setColor(color);
 	}
-	
-	public void Undo() {
-		((ActivityRenderingModel) this.model).setColor(oldColor);
-	}
-
 }
