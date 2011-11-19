7c5b158f01485a82f9ae7a383027af54659a5f86
cyril.voegtlin <cyril.voegtlin@localhost>
2011-11-19 20:52:48 +0100

    Determined what is in a selectRectangle

--- src/sequence/utilities/SelectRectangle.java
+++ src/sequence/utilities/SelectRectangle.java
@@ -1,5 +1,7 @@
 package sequence.utilities;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -12,12 +14,20 @@ import javax.swing.JPanel;
 
 public class SelectRectangle extends JPanel implements MouseMotionListener, MouseListener{
 	private static final long serialVersionUID = 1L;
-	Rectangle selection;
-	Point anchor;
+	private Rectangle selection;
+	private Point anchor;
+	private Container context;
 	 
 	public SelectRectangle(){
 		addMouseListener(this);
 		addMouseMotionListener(this);
+		this.context = getParent();
+	}
+	
+	public SelectRectangle(Container context){
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		this.context = context;
 	}
 	 
 	protected void paintComponent(Graphics g) {
@@ -27,15 +37,35 @@ public class SelectRectangle extends JPanel implements MouseMotionListener, Mous
 			g2d.draw(selection);
 		}
 	}
-	 
+	
+	public boolean contains(Component c) {
+		for(int i=0 ; i<context.getComponentCount() ; i++) {
+			if(c.equals(context.getComponent(i)))
+				return true;
+		}
+		return false;
+	}
+	
+	private void selectComponents(Point p) {
+		Component component = context.getComponentAt(p);
+		if(!contains(component) && !component.equals(context))
+			add(component);
+		for(int i=0 ; i<getComponentCount() ; i++) {
+			if(!selection.intersects(getComponent(i).getBounds()));
+			remove(getComponent(i));
+		}
+	}
+	
 	public void mousePressed(MouseEvent e) {
 		anchor = e.getPoint();
 		selection = new Rectangle(anchor);
+		selectComponents(e.getPoint());
 	}
 	 
 	public void mouseDragged(MouseEvent e) {
 		selection.setBounds( (int)Math.min(anchor.x,e.getX()), (int)Math.min(anchor.y,e.getY()),
 		(int)Math.abs(e.getX()-anchor.x), (int)Math.abs(e.getY()-anchor.y));
+		selectComponents(e.getPoint());
 		repaint();
 	}
 	 
