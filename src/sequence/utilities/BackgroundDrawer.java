b40c0278421e21562888debbdc9ced9c669fb4d9
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-12-02 16:34:40 +0100

    Create and use BackgroundDrawer.

---
+++ src/sequence/utilities/BackgroundDrawer.java
@@ -0,0 +1,24 @@
+package sequence.utilities;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class BackgroundDrawer {
+	private Color color;
+	public BackgroundDrawer() {
+		this.color=Color.BLACK;
+	}
+	public BackgroundDrawer(Color color) {
+		this.color=color;
+	}
+	public void Draw(Graphics2D g, int width, int height)
+	{
+		Draw(g, width, height, this.color);
+	}
+	public void Draw(Graphics2D g, int width, int height, Color color)
+	{
+		g.setColor(color);
+		g.fillRect(0, 0, width, height);
+	}
+
+}
