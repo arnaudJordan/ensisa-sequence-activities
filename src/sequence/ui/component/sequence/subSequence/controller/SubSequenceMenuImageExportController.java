0adcfa2a6258b12802353cf3b9811ed57abe8902
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2011-12-02 15:06:37 +0100

    Add message dialog when user try to save image with unsupported extension.

--- src/sequence/ui/component/sequence/subSequence/controller/SubSequenceMenuImageExportController.java
+++ src/sequence/ui/component/sequence/subSequence/controller/SubSequenceMenuImageExportController.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import sequence.mvc.Controller;
@@ -33,21 +34,52 @@ public class SubSequenceMenuImageExportController extends Controller implements
     		if(config.getLastOpenedDirectory() !=null)
     			fc.setCurrentDirectory(config.getLastOpenedDirectory());
     		fc.setMultiSelectionEnabled(true);
-    		int returnVal = fc.showSaveDialog(getView());
-			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				config.setLastOpenedDirectory(fc.getCurrentDirectory());
-				try {
-					Config.serialize(config);
+    		boolean correctExt=false;
+    		do
+    		{
+	    		int returnVal = fc.showSaveDialog(getView());
+	    		if(returnVal == JFileChooser.CANCEL_OPTION) correctExt=true;
+				if(returnVal == JFileChooser.APPROVE_OPTION)
+				{
+					File f = fc.getSelectedFile();
+					int mid = f.getName().lastIndexOf('.') + 1;
+					String ext = f.getName().substring(mid);
+					
+					for(int i=0; i<ImageIO.getWriterFormatNames().length; i++)
+					{
+						if(ImageIO.getWriterFormatNames()[i].equals(ext))
+						{
+							correctExt=true;
+						}
+					}
+					if(correctExt)
+					{
+						ImageIO.write(getView().createImage(), ext, f);
+						
+						config.setLastOpenedDirectory(fc.getCurrentDirectory());
+						try {
+							Config.serialize(config);
+						}
+						catch (java.io.IOException ex) {
+							ex.printStackTrace();
+						}
+					}
+					else
+					{
+						StringBuilder sb=new StringBuilder();
+						for(int i=0; i<ImageIO.getWriterFormatNames().length; i++)
+						{
+							sb.append(ImageIO.getWriterFormatNames()[i].toString());
+							sb.append(" ");
+						}
+						ImageIO.getWriterFormatNames().toString();
+						JOptionPane.showMessageDialog(getView(),
+								"Supported file types are : "+sb,
+								"File type not supported",
+								JOptionPane.ERROR_MESSAGE);
+					}
 				}
-				catch (java.io.IOException ex) {
-					ex.printStackTrace();
-				}
-				File f = fc.getSelectedFile();
-				int mid = f.getName().lastIndexOf('.') + 1;
-				String ext = f.getName().substring(mid);
-				
-				ImageIO.write(getView().createImage(), ext, f);
-			}
+    		}while(!correctExt);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
