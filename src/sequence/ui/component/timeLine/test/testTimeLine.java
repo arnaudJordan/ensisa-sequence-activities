06bf34050c5e35139cf12fad5367d86ff21be8fb
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2012-01-11 22:49:08 +0100

    testTimeLine better implementation.

--- src/sequence/ui/component/timeLine/test/testTimeLine.java
+++ src/sequence/ui/component/timeLine/test/testTimeLine.java
@@ -1,31 +1,49 @@
 package sequence.ui.component.timeLine.test;
 
 import java.awt.EventQueue;
-import javax.swing.JFrame;
+import java.io.File;
+import java.io.IOException;
 
-import sequence.model.Phase;
+import javax.swing.JFrame;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
 import sequence.model.Phases;
+import sequence.model.Sequence;
+import sequence.parser.SequenceHandler;
 import sequence.ui.component.timeLine.TimeLineView;
 
 public class testTimeLine extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public testTimeLine() {
-			
-			Phases model = new Phases();
-			Phase phase = new Phase(10);
-			phase.setStopTime(20);
-			model.add(phase);
-			phase = new Phase(30);
-			phase.setStopTime(60);
-			model.add(phase);
-			
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+
+		SAXParser parser;
+		try {
+			parser = factory.newSAXParser();
+			File parsedFile = new File("src/data.xml");
+			SequenceHandler sequenceHandler = new SequenceHandler();
+			parser.parse(parsedFile, sequenceHandler);
+
+			Sequence sequence = sequenceHandler.getSequence();
+			Phases model = sequence.getPhases();
+			System.out.println(model);
 			TimeLineView view = new TimeLineView(model);
 
 			this.add(view);
 			this.pack();
 			this.setVisible(true);
-
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
