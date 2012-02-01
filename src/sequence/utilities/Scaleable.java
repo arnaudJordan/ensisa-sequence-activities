1f25ef725961797c66035f7ac5f7705652eca8ba
aj.arnaud.jordan <aj.arnaud.jordan@localhost>
2012-02-02 00:29:51 +0100

    Add some javadoc and improve timeLayout implementation.

--- src/sequence/utilities/Scaleable.java
+++ src/sequence/utilities/Scaleable.java
@@ -1,6 +1,24 @@
 package sequence.utilities;
 
+/**
+ * Defines the interface for scaleable classes.
+ *
+ * @see Timeable
+ * @see sequence.ui.utilities.TimeLayout
+ */
 public interface Scaleable {
+	
+	/**
+	 * Gets the h scale.
+	 *
+	 * @return the h scale
+	 */
 	public float getHScale();
+	
+	/**
+	 * Gets the v scale.
+	 *
+	 * @return the v scale
+	 */
 	public float getVScale();
 }
--- src/sequence/utilities/Timeable.java
+++ src/sequence/utilities/Timeable.java
@@ -1,7 +1,31 @@
 package sequence.utilities;
 
+/**
+ * Defines the interface for time positionned classes.
+ *
+ * @see Scaleable
+ * @see sequence.ui.utilities.TimeLayout
+ */
 public interface Timeable {
+	
+	/**
+	 * Gets the start time.
+	 *
+	 * @return the start time
+	 */
 	public int getStartTime();
+	
+	/**
+	 * Gets the stop time.
+	 *
+	 * @return the stop time
+	 */
 	public int getStopTime();
+	
+	/**
+	 * Gets the duration.
+	 *
+	 * @return the duration
+	 */
 	public int getDuration();
 }
