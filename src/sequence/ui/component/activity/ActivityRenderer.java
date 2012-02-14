a619a58ceedbd1308b95421ac1ea511d80fcde29
cyril.voegtlin <cyril.voegtlin@localhost>
2012-02-14 19:48:04 +0100

    Deleted unused class.

--- src/sequence/ui/component/activity/ActivityRenderer.java
+++ src/sequence/ui/component/activity/ActivityRenderer.java
@@ -1,5 +1,6 @@
 package sequence.ui.component.activity;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 
@@ -13,25 +14,67 @@ public class ActivityRenderer extends DefaultRenderer implements Renderer {
 	public static BackgroundDrawer CURRENT_BACKGROUND_DRAWER = new BackgroundDrawer();
 	
 	protected BackgroundDrawer bd;
-	
+	private boolean contracted;
+
 	public ActivityRenderer(View view) {
 		super(view);
-		this.bd=CURRENT_BACKGROUND_DRAWER;
+		this.bd = CURRENT_BACKGROUND_DRAWER;
+		this.contracted = false;
 	}
-
+	
 	public void renderView(Graphics2D g) {
-		super.renderView(g);
         renderActivity(g);
 	}
 	
 	private void renderActivity(Graphics2D g) {
 		renderBackground(g);
 	}
-
+	
 	private void renderBackground(Graphics2D g) {
 		Activity activity = (Activity) ((ActivityView)getView()).getModel();
-		if(activity != null)
-			bd.Draw(g, (int)(getView().getSize().getWidth()), (int)(getView().getSize().getHeight()), ((ActivityRenderingModel)getView().getRenderingModel()).getColor());
+		g.setColor(((ActivityRenderingModel)getView().getRenderingModel()).getColor());
+		if(activity == null)
+			return;
+		
+		ActivityRenderingModel renderingModel = ((ActivityRenderingModel)getView().getRenderingModel());
+		int parentWidth = getView().getParent().getWidth();
+		int activityWidth = (int) (activity.getActivitytime().getDuration() * renderingModel.getHScale());
+		bd.Draw(g, activityWidth, (int) (renderingModel.getHeight() * renderingModel.getVScale()), ((ActivityRenderingModel)getView().getRenderingModel()).getColor());
+		if(parentWidth<activityWidth)
+		{
+			this.contracted = true;
+			g.drawLine(parentWidth/2 -5 , -5, parentWidth/2 +5, (int) getView().getSize().getHeight()+5);
+			drawCutMark(g, parentWidth/2);
+		}
+		else
+			this.contracted = false;
+		//g.setColor(Color.WHITE);
+		//g.drawString(String.valueOf(activity.getId()), (int) getView().getSize().getWidth()/2, (int) getView().getSize().getHeight()/2);
+	}
+	
+	private void drawCutMark(Graphics2D g, int middle)
+	{
+		Graphics2D g2 = (Graphics2D) g.create();
+		g2.setColor(Color.WHITE);
+		final int gap = 5;
+		g2.drawLine(middle - 2*gap , 0, middle, (int) getView().getSize().getHeight()+5);
+		g2.drawLine(middle , 0, middle + 2*gap, (int) getView().getSize().getHeight()+5);
+	}
+	
+	public Dimension getPreferredSize() {
+		Activity activity = (Activity) ((ActivityView)getView()).getModel();
+		ActivityRenderingModel renderingModel = ((ActivityRenderingModel)getView().getRenderingModel());
+		if(activity != null && renderingModel != null)
+		{
+			int parentWidth = getView().getParent().getWidth();
+			int activityWidth = (int) (activity.getActivitytime().getDuration() * renderingModel.getHScale());
+			if(parentWidth<activityWidth)
+				return new Dimension(parentWidth, (int) (renderingModel.getHeight() * renderingModel.getVScale()));
+			else
+				return new Dimension(activityWidth, (int) (renderingModel.getHeight() * renderingModel.getVScale()));
+		}
+			
+		return super.getPreferredSize();
 	}
 	
 	public void setBackgroundDrawer(BackgroundDrawer bd)
@@ -39,16 +82,12 @@ public class ActivityRenderer extends DefaultRenderer implements Renderer {
 		this.bd=bd;
 		getView().repaint();
 	}
+	
 	public BackgroundDrawer getBackgroundDrawer() {
 		return this.bd;
 	}
-	public Dimension getPreferredSize() {
-		Activity activity = (Activity) ((ActivityView)getView()).getModel();
-		ActivityRenderingModel renderingModel = ((ActivityRenderingModel)getView().getRenderingModel());
-		if(activity != null && renderingModel != null)
-			return new Dimension((int)(activity.getActivitytime().getDuration() * renderingModel.getHScale()), (int)(((ActivityRenderingModel)getView().getRenderingModel()).getHeight() * renderingModel.getVScale()));
-		return super.getPreferredSize();
-	}
-
 	
+	public boolean isContracted() {
+		return contracted;
+	}
 }
