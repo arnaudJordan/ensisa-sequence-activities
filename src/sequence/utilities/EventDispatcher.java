6c2dd68eb2158e14d9d502d7e4a6e6431a4f682f
cyril.voegtlin <cyril.voegtlin@localhost>
2012-02-15 19:47:27 +0100

    Added scale listener.

--- src/sequence/utilities/EventDispatcher.java
+++ src/sequence/utilities/EventDispatcher.java
@@ -4,22 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import sequence.ui.component.activity.ActivityRenderer;
+import sequence.ui.component.activity.ActivityRenderingModel;
 import sequence.ui.utilities.drawer.BackgroundDrawer;
 
 public class EventDispatcher {
-	private static List<BackgroundListener> listeners = new ArrayList<BackgroundListener>();
-
+	private static List<BackgroundListener> backgroundListeners = new ArrayList<BackgroundListener>();
+	private static List<ScaleListener> scaleListeners = new ArrayList<ScaleListener>();
+	
 	public static void add(BackgroundListener bl) {
-		listeners.add(bl);
+		backgroundListeners.add(bl);
 	}
 	
 	public static void remove(BackgroundListener bl) {
-		listeners.remove(bl);
+		backgroundListeners.remove(bl);
 	}
 	
 	public static void backgroundChanged(BackgroundDrawer bd) {
 		ActivityRenderer.CURRENT_BACKGROUND_DRAWER = bd;
-		for(BackgroundListener bl : listeners)
+		for(BackgroundListener bl : backgroundListeners)
 			bl.backgroundChanged(bd);
+	}
+	
+	public static void add(ScaleListener sl) {
+		scaleListeners.add(sl);
+	}
+	
+	public static void remove(ScaleListener sl) {
+		scaleListeners.remove(sl);
+	}
+	
+	public static void scaleChanged(float scale) {
+		ActivityRenderingModel.CURRENT_SCALE = scale;
+		for(ScaleListener sl : scaleListeners)
+			sl.scaleChanged(scale);
 	}
 }
